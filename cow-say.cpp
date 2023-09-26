0f49b2cfe2392fb6447ff0c17d0152227e07d371
Emanuel Acosta <emanuelacosta809@gmail.com>
2023-09-25 20:54:48 -0400

    added cow-say.cpp

---
+++ cow-say.cpp
@@ -0,0 +1,58 @@
+/* open cow-say, an Opensource alternative to cowsay by Emanvidmaker
+  ___
+< moo >
+  ---
+         \   ^__^ 
+          \  (oo)\_______
+             (__)\       )\/\\
+                 ||----w |
+                 ||     ||
+*/
+#include <iostream>
+void place(int n, char c){
+    if (n <= 0) return;
+    else {
+        std::cout << c; 
+        place(n-1,c);
+        return;
+    }
+}
+void place(int n = 1) {place(n,' ');};
+int main(int argCount,char *argv[]) {
+    std::string cowSays = "";
+    if (argCount-1 <= 0) {
+        cowSays = "moo";
+    } else {
+        for (int i = 1; i < argCount; i++)
+        {
+            cowSays += argv[i];
+            if (i < argCount-1){
+                cowSays += ' ';
+            }
+        }
+    }
+    int length = cowSays.length();
+    place(2);
+    place(length,'_');
+    std::cout << std::endl;
+    std::cout << "< " << cowSays << " >"<< std::endl;
+    place(2);
+    place(length,'-');
+    std::cout << std::endl;
+    //draw cow
+    place(length+3);
+    std::cout << "\\   ^__^ "<< std::endl;
+    place(length+4);
+    std::cout << "\\  (oo)\\_______"<< std::endl;
+    place(length+7);
+    std::cout << "(__)\\       )\\/\\"<< std::endl;
+    place(length+11);
+    std::cout << "||----w |"<< std::endl;
+    place(length+11);
+    std::cout << "||     ||"<< std::endl;
+    place(length+24,'-');
+    std::cout << "\nOpen cow-say, an Opensource alternative to cowsay"<< std::endl;
+
+   return 0;
+}
+
